953c757f5369bea1c3366303e721cf1b8a5eb0f2
Yinxian <sm141316122@gmail.com>
2023-04-06 15:11:37 +0800

    update search funtion

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
+import os
+import json
+
+
+def search():
+    website = web_input.get().title()
+    if os.path.isfile("data.json"):
+        with open("data.json") as file:
+            data = json.load(file)
+            if website == "":
+                messagebox.showwarning("Failed", "請輸入要查詢的網站")
+            elif website in data.keys():
+                user = data[website]["email"]
+                password = data[website]["password"]
+                messagebox.showinfo("Successful !", f"{website}\nUsername/Email: {user}\nPassword: {password}")
+            else:
+                messagebox.showwarning("Failed", f"沒有 {website} 網站的資訊")
+    else:
+        messagebox.showwarning("Failed", "尚未建立清單")
+
+    web_input.delete(0, END)
 
 
 def generate_password():
@@ -30,10 +51,25 @@ def add():
     website = web_input.get().title()
     user_name = user_name_input.get()
     password = password_input.get()
+    new_data = {
+        website: {
+            "email": user_name,
+            "password": password
+        }
+    }
 
     if website and user_name and password != "":
-        with open("data.txt", "a") as file:
-            file.write(f"{website} | {user_name} | {password}\n")
+        if os.path.isfile("data.json"):
+            with open("data.json", "r") as file:
+                data = json.load(file)
+                data.update(new_data)
+
+            with open("data.json", "w") as file:
+                json.dump(data, file, indent=4)
+        else:
+            with open("data.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+
         messagebox.showinfo("Successful !", "已加入清單")
     else:
         messagebox.showwarning("Failed", "未輸入完成, 請重新輸入")
@@ -63,8 +99,8 @@ password_label = Label(text="Password:")
 password_label.config(pady=10)
 password_label.grid(row=3, column=0)
 
-web_input = Entry(width=45)
-web_input.grid(row=1, column=1, columnspan=2)
+web_input = Entry(width=28)
+web_input.grid(row=1, column=1)
 user_name_input = Entry(width=45)
 user_name_input.insert(0, "123456@gmail.com")
 user_name_input.grid(row=2, column=1, columnspan=2)
@@ -75,5 +111,7 @@ gen_password_button = Button(text="Generate Password", command=generate_password
 gen_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=45, command=add, bg="white", highlightthickness=0)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", width=15, command=search, bg="white", highlightthickness=0)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
